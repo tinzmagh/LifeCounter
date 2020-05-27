811ecb8e631871ff16fdef428075073c301ad010
Martine <martinemagh@gmail.com>
2020-05-27 11:17:53 +0200

    CI

---
+++ src/test/java/mandatory/test/DataProvider.java
@@ -0,0 +1,17 @@
+package mandatory.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DataProvider {
+
+    ArrayList<Integer> amountOfPlayers = new ArrayList<Integer>( Arrays.asList(2, 3, 4, 5, 6));
+    ArrayList<Integer> startingLife = new ArrayList<Integer>( Arrays.asList(1, 10, 501, 1));
+
+
+
+    ArrayList<Integer> invalidInputs = new ArrayList<Integer>( Arrays.asList(-1, 0, 7));
+
+
+
+}
